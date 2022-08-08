d958424d22759b1ea0a186339c4f29147c36b92c
Jess <110465658+jessical2@users.noreply.github.com>
2022-08-08 20:55:35 +0100

    Input fields added

--- src/app.py
+++ src/app.py
@@ -3,10 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+# @app.route("/addrec", methods = ["POST", 'GET'])
+# def addrec():
+#     return
+    # if request.method == 'POST':
+
+
+@app.route("/booked")
+def booked():
+    return render_template("booked.html")
+
 if __name__== "__main__":
     app.run()
+
+
