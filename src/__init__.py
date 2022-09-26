687c01a54549780d697bb989d547832e1039d463
Jess <110465658+jessical2@users.noreply.github.com>
2022-09-26 10:42:15 +0100

    fix spelling mistake

--- src/__init__.py
+++ src/__init__.py
@@ -44,7 +44,7 @@ def create_app():
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-aut
+    # blueprint for main
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
